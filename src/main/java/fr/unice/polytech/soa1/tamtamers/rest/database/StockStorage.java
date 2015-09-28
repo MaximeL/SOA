1efe0ad099617d4530ff9a3ce4d488301f48a9b5
Sébastien <s.gregoire@outlook.com>
2015-09-28 17:38:16 +0200

    Init gestion de stock

--- src/main/java/fr/unice/polytech/soa1/tamtamers/rest/database/StockStorage.java
+++ src/main/java/fr/unice/polytech/soa1/tamtamers/rest/database/StockStorage.java
@@ -1,16 +1,32 @@
 package fr.unice.polytech.soa1.tamtamers.rest.database;
 
+import fr.unice.polytech.soa1.tamtamers.rest.entity.StockItem;
 import fr.unice.polytech.soa1.tamtamers.rest.entity.Tamtam;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Random;
 
-/**
- * Created by sgregoire on 28/09/2015.
- */
 public class StockStorage {
-    private static HashMap<Integer, Tamtam> tamtams = new HashMap<Integer, Tamtam>();
+    private static HashMap<Integer, StockItem> stockItems = new HashMap<Integer, StockItem>();
 
-    public static findAllStockItems()
+    public static Collection<StockItem> findAllStockItems() {
+        return stockItems.values();
+    }
 
+    public static StockItem findStockItem(Integer id) {
+        return stockItems.get(id);
+    }
 
+    static {
+        Collection tamtams = TamtamStorage.findAllTamtams();
+        StockItem item;
+        Random random = new Random();
+        for(Tamtam tamtam : tamtams) {
+            item = new StockItem();
+            item.setItemId(tamtam.getId());
+            item.setNumberInStock(random.nextInt(100));
+        }
+        // TODO : Initialiser le stock
+    }
 }
