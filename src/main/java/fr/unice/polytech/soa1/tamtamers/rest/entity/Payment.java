b1e5dc099e9abfcad950ad26acda4da16b614d6d
Sébastien <s.gregoire@outlook.com>
2015-09-30 19:41:48 +0200

    Gestion du CB & Paypal

--- src/main/java/fr/unice/polytech/soa1/tamtamers/rest/entity/Payment.java
+++ src/main/java/fr/unice/polytech/soa1/tamtamers/rest/entity/Payment.java
@@ -1,12 +1,13 @@
 package fr.unice.polytech.soa1.tamtamers.rest.entity;
 
-public class Paiement {
+public class Payment {
     private int order;
     private float amount;
     private String transaction;
     private Status status;
+    private Type type;
 
-    public Paiement(int order) {
+    public Payment(int order) {
         this.order = order;
         this.status = Status.UNKNOWED;
     }
@@ -43,13 +44,36 @@ public class Paiement {
         this.status = status;
     }
 
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
     public enum Status {
         UNKNOWED("Inconnu"),
-        VALID("Validé"),
-        DECLINE("Refusé");
+        VALID("ValidÃ©"),
+        DECLINE("RefusÃ©");
 
         private final String val;
         Status(String val) {
+            this.val = val;
+        }
+
+        @Override
+        public String  toString() {
+            return val;
+        }
+    }
+
+    public enum Type {
+        PAYPAL("PayPal"),
+        CB("Credit Card");
+
+        private final String val;
+        Type(String val) {
             this.val = val;
         }
 
