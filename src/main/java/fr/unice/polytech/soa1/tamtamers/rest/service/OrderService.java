1fc8814b9fcc43cc6fee30da23dcc6d02809e579
MaximeL <lozach-maxime@laposte.net>
2015-10-04 10:46:01 +0200

    Upgrade of the doc

--- src/main/java/fr/unice/polytech/soa1/tamtamers/rest/service/OrderService.java
+++ src/main/java/fr/unice/polytech/soa1/tamtamers/rest/service/OrderService.java
@@ -6,15 +6,13 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 // TODO : Sebastien Pas de verbe
 
@@ -55,24 +53,8 @@ public class OrderService {
     }
 
     /**
-     * (GET) Return all the orders of an user
-     * @param id  int  (PATH)  id of the user
-     * @return Response JSon format
-     */
-    @GET
-    @Path("/{user}")
-    public Response getUserOrders(@PathParam("user") int id) {
-        Collection<Order> orders = OrderStorage.getUserOrders(id);
-        JSONArray result = new JSONArray();
-        for(Order order: orders) {
-            result.put(new JSONObject(order.toString()));
-        }
-        return Response.ok().entity(result.toString()).build();
-    }
-
-    /**
      * (GET) Get all the order that are in a given state
-     * @param state  String  (PATH)  Name of the state
+     * @param state  String  (QUERY)  Name of the state
      * @return Response JSon format
      */
     @GET
@@ -84,6 +66,18 @@ public class OrderService {
             result.put(new JSONObject(order.toString()));
         }
         return Response.ok().entity(result.toString()).build();
+    }
+
+    /**
+     * (POST) Change the state of an order to the next state
+     * @param id  int  (QUERY)  id of the order
+     * @return Response JSon format
+     */
+    @POST
+    @Path("/status")
+    public Response getStateOrders(@QueryParam("id") int id) {
+        OrderStorage.getOrder(id).nextStatus();
+        return Response.ok().build();
     }
 
 }
